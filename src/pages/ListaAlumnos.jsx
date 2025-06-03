3ebbb62cdc2c235d952fe97269cc883da281b711
CarlosFigueroa497 <carlos.4760230@gmail.com>
2025-06-03 19:57:12 -0300

    Agregue mensajito al eliminar con exito

--- src/pages/ListaAlumnos.jsx
+++ src/pages/ListaAlumnos.jsx
@@ -1,17 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 
 function ListaAlumnos({ alumnos, setAlumnos }) {
+  const [mensajeExito, setMensajeExito] = useState("");
   const handleEliminar = (id) => {
     const confirmar = confirm('¿Esta seguro que desea eliminar este alumno?');
     if (confirmar) {
       const nuevosAlumnos = alumnos.filter((alumno) => alumno.id !== id);
       setAlumnos(nuevosAlumnos);
+      setMensajeExito("✅ Alumno eliminado con éxito");
+    setTimeout(() => setMensajeExito(""), 3000); // Se borra en 3 segundos
     }
   };
 
   return (
   <div style={{ textAlign: 'center' }}>
     <h2>Lista de Alumnos</h2>
+    {mensajeExito && (
+  <div style={{
+    backgroundColor: "#d4edda",
+    color: "#155724",
+    padding: "10px",
+    borderRadius: "5px",
+    marginBottom: "15px",
+    border: "1px solid #c3e6cb"
+  }}>
+    {mensajeExito}
+  </div>
+)}
     {alumnos.length === 0 ? (
       <p>No hay alumnos cargados.</p>
     ) : (
