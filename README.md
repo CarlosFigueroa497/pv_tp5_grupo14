d8e6233f28105c819be9ba3e4a72bd9029f407b7
CarlosFigueroa497 <carlos.4760230@gmail.com>
2025-06-03 22:08:05 -0300

    Agregue informacion en AcercaDe y README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pv_tp5_grupo14
 Pv2025 tp5  Eventos – React Router Dom
+
+Integrantes:
+Figueroa Carlos Augusto | github: CarlosFigueroa497
+Briones Juan Nicolas | github: juanicolas619
+Tapia Moraña Hernan Jairo | github: HernanJairoMT
+
+
+Este proyecto consiste en el desarrollo de una aplicación web construida con React y Vite, cuyo objetivo principal es gestionar datos de alumnos. La aplicación permite realizar operaciones básicas como agregar, editar, eliminar y visualizar información detallada de cada alumno.
+
+Está organizada en componentes funcionales y utiliza React Router DOM para la navegación entre vistas. El manejo de eventos se realiza a través de eventos sintéticos de React y funciones personalizadas que permiten una comunicación efectiva entre componentes.
+
+caracteristicas del proyecto:
+
+•Navegación fluida entre vistas como Inicio, Lista de alumnos, Agregar alumno, Editar alumno y Acerca de.
+•Formularios controlados con validación básica.
+•Rutas dinámicas y redirecciones utilizando las herramientas de React Router.
+•Separación de responsabilidades mediante componentes reutilizables.
+•Estilo uniforme y diseño adaptable.
+
--- src/pages/AcercaDe.jsx
+++ src/pages/AcercaDe.jsx
@@ -1,4 +1,25 @@
 function AcercaDe() {
-  return <h2>Acerca De los Creadores</h2>
+  return (
+    <div style={{ padding: "1rem" }}>
+      <h2>Acerca de los Colaboradores</h2>
+      <p>Trabajo Práctico N°5 - Programación Visual 2025</p>
+      <p>Carrera: Analista Programador Universitario</p>
+      <p>Materia: Programación Visual</p>
+      <p>Fecha de Entrega: Hasta el miercoles 4 de junio a las 22 hs</p>
+
+      <h3 style={{ marginTop: "1rem" }}>Integrantes del Grupo 14:</h3>
+      <ul>
+        <li>Carlos Figueroa – GitHub: <a href="https://github.com/CarlosFigueroa497" target="_blank" rel="noreferrer">CarlosFigueroa497</a></li>
+        <li>Briones Juan Nicolas – GitHub: <a href="https://github.com/juanicolas619" target="_blank" rel="noreferrer">juanicolas619</a></li>
+        <li>Tapia Moraña Hernan – GitHub: <a href="https://github.com/HernanJairoMT" target="_blank" rel="noreferrer">HernanJairoMT</a></li>
+      </ul>
+
+      <p style={{ marginTop: "1rem" }}>
+        Esta aplicación fue desarrollada como parte del Trabajo Práctico N°5, utilizando React, Vite y React Router DOM, cumpliendo con todos los requerimientos solicitados por la cátedra.
+      </p>
+    </div>
+  );
 }
-export default AcercaDe
+
+
+export default AcercaDe;
